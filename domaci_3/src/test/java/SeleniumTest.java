77a6233ee48947300adfb77b2779e9a4e5b6aaff
dzaja123 <nikolacardzajevic@gmail.com>
2022-07-17 13:27:24 +0200

    Дорада кода

--- domaci_3/src/test/java/SeleniumTest.java
+++ domaci_3/src/test/java/SeleniumTest.java
@@ -3,13 +3,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class SeleniumTest {
 
