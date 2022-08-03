65648fd973a7709c419a6077a75fc2372dd91638
Nikola Džajević <44314786+dzaja123@users.noreply.github.com>
2022-08-03 16:58:15 +0200

    Урађене све измене према захтеву

--- domaci_4/src/test/java/TestRegistration.java
+++ domaci_4/src/test/java/TestRegistration.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -7,22 +8,34 @@ import org.testng.annotations.Test;
 public class TestRegistration {
 
     private String testEmail = "kurs_endava_test_mail@gmail.com";
+    
     private String testPassword = "Nekarandomsifra123";
-    private RegisterPage registerPage;
+
+    private String testUsername = "kursEndavaTestUsername";
+
+    WebDriver driver;
+
+    RegisterPage registerPage;
+
+    LogoutPage logoutPage;
 
     @BeforeMethod
     @Parameters({"browser"})
     public void setUp(String browser) {
 
-        registerPage = TestBase.setUpWebBrowser(browser);
+        driver = TestBase.setUpWebBrowser(browser);
     }
 
     @Test
     public void testStartingWebBrowser(){
 
-        registerPage.openWebPage();
+        registerPage = new RegisterPage(driver);
+        logoutPage = new LogoutPage(driver);
 
-        registerPage.insertEmailAndPassword(testEmail, testPassword);
+        logoutPage.openWebPage();
+
+        registerPage.insertEmailAndPasswordAndUsername(testEmail, testPassword, testUsername);
+
 
     }
 
